4864625cd01e009b1ff5929664ccb7d1bd5a0c6f
jaydeep apte <jaydeepresources@gmail.com>
2019-06-17 11:34:11 +0530

    Add files via upload

---
+++ numbers/main.js
@@ -0,0 +1,32 @@
+
+function increase() {
+    var no = document.getElementById('spanId').innerText;
+    no++;
+    document.getElementById('spanId').innerText = no;
+    setColor();
+}
+
+function decrease() {
+    document.getElementById('spanId').innerText--;
+    setColor();
+}
+
+function setColor() {
+    var idSpan = document.getElementById('spanId');
+    var idColor = idSpan.innerText;
+
+    // if (idColor == 0) {
+    //     idSpan.classList.replace(idSpan.classList.item(0), 'blue');
+    // }
+    // else if (idColor > 0) {
+    //     idSpan.classList.replace(idSpan.classList.item(0), 'green');
+    // }
+    // else if (idColor < 0) {
+    //     idSpan.classList.replace(idSpan.classList.item(0), 'red');
+    // }
+
+    (idColor == 0) ? idSpan.classList.replace(idSpan.classList.item(0), 'blue') : 
+    (idColor > 0) ? idSpan.classList.replace(idSpan.classList.item(0), 'green') : 
+    idSpan.classList.replace(idSpan.classList.item(0), 'red');
+
+}
